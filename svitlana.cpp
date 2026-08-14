ffeb5671e2f5949cb9b9ae43f5e26a53687697fe
svitlanadp12 <svitlana.dp@gmail.com>
2026-08-14 18:46:00 +0300

    add new file svitlana.cpp

---
+++ svitlana.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+void printValue(int* x, int* y)
+
+int main()
+{
+    int a = 5;
+    int b = 10;
+    cout << "to swap a=" << a << ", b=" << b << endl;
+    swapNumbers(&a, &b);
+    cout << "After swap: a = " << a << ", b= " << b << endl;
+    return 0;
+}
